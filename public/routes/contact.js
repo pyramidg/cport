3198e3ebe2508e3c08f9b4e266790fdb2d02b9b1
studioplace <webstudioplace.com>
2016-12-13 13:17:39 -0500

    moved react module

---
+++ public/routes/contact.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/contact', function(req, res, next) {
+res.render('contact', {
+  title: 'Contact', 
+  pageID: 'contact'
+});
+
+
+
+});
+
+module.exports = router;
